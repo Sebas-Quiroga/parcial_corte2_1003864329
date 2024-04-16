4c6f6470954840aca84f41788b375e1d93d83a34
Sebas-Quiroga <125420726+Sebas-Quiroga@users.noreply.github.com>
2024-04-16 07:04:05 -0500

    Finalizacion

--- frontend/Consultoria/src/pages/Home.tsx
+++ frontend/Consultoria/src/pages/Home.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+// src/pages/AddPatient.tsx
 
-const Home: React.FC = () => {
+import { IonButton, IonContent, IonDatetime, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+
+const AddPatient: React.FC = () => {
+  const [patientName, setPatientName] = useState<string>('');
+  const [patientDOB, setPatientDOB] = useState<string>('');
+  const [doctorName, setDoctorName] = useState<string>('');
+  const [doctorSpecialty, setDoctorSpecialty] = useState<string>('');
+
+  const history = useHistory();
+
+  const addPatient = () => {
+    // Aquí puedes agregar la lógica para guardar el paciente
+    // Por ejemplo, puedes enviar los datos a un servidor o guardarlos localmente
+
+    // Puedes agregar más lógica aquí, como navegar a otra página o limpiar los campos
+    history.push('/patient-list');
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>LOGGINVILLA</IonTitle>
+          <IonTitle>consulta</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">LOGGINVILLA</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonInput placeholder="Nombre del paciente" value={patientName} onIonChange={e => setPatientName(e.detail.value!)}></IonInput>
+        <IonInput placeholder="Nombre del médico" value={doctorName} onIonChange={e => setDoctorName(e.detail.value!)}></IonInput>
+        <IonInput placeholder="Especialidad del médico" value={doctorSpecialty} onIonChange={e => setDoctorSpecialty(e.detail.value!)}></IonInput>
+        <IonButton onClick={addPatient}>Agregar</IonButton>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Home;
+export default AddPatient;
 
